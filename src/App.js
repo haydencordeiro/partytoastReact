ef361820b0aaa9ae560606c941565c1b1787362c
Hayden Corderio <cordeirohayden@gmail.com>
2021-01-21 15:10:14 +0530

    trail 4

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Navbar></Navbar>
     <Switch>
       <Route exact component={ProductDetails} path='/product/:slug'  />
